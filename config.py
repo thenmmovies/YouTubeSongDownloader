978ccd856968de6f7787d8a6621dd78e0048fb77
thenmmovies <97659554+thenmmovies@users.noreply.github.com>
2022-03-10 21:39:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "2658292"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "3844e31291e5d5b4f8441f07a6a7b812" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "5246127227:AAEaWxA0lyuf4vIO-BYuWTFjPbz1wxRG594" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
